57eb9f40c706a079c65e3397c6c723da0198ae3e
Tomasz Kaniewski <tomaszkaniewski95@gmail.com>
2017-11-05 12:58:55 +0100

    Add generated code from Visual Studio

---
+++ bookbox/bookbox/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bookbox.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
